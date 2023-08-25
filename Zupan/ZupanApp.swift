5c0469db78a6a652ffdb55368e23469375d434ec
JoaquinDuranPeya <joaquin.duran@pedidosya.com>
2023-08-25 16:02:31 -0300

    Initial Commit

---
+++ Zupan/ZupanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZupanApp.swift
+//  Zupan
+//
+//  Created by Joaquin Duran on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZupanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
